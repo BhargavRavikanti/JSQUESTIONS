5688293a1bd47176069897a21ac534cee86c43f1
BhargavRavikanti20 <bhargavravikanti20@gmail.com>
2023-11-02 23:24:39 +0530

    basics practice

--- JONAS/JONAS Basics.js
+++ JONAS/JONAS Basics.js
@@ -1019,23 +1019,29 @@ Encapsulation in JavaScript: In JavaScript, encapsulation can be achieved using 
 // 2) Private fields
 // 3) Public methods
 // 4) Private methods
+// (there is also the static version)
 
+/*
 class Account {
   // 1) Public fields (instances) which means there are all not on the prototype
   locale = navigator.language;
 
-  // 2) Private fields
+  // 2) Private fields (instances)
   #movements = [];
+  #pin;
 
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
-    // this._movements = []; // protected
+    // protected property
+    this.#pin = pin;
+    // this._movements = [];
     // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
+
+  // 3) Public methods
 
   // Public interface
   getMovements() {
@@ -1050,15 +1056,30 @@ class Account {
     this.deposit(-val);
   }
 
-  _approveLoan(val) {
-    return true;
-  }
+  // requestLoan(val) {
+  //   if (this.#approveLoan(val)) {
+  //     this.deposit(val);
+  //     console.log("Loan Approved");
+  //   }
+  // }
 
   requestLoan(val) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log("Loan Approved");
     }
+  }
+
+  static helper() {
+    console.log("Helper");
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  //   return true;
+  // }
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -1075,16 +1096,120 @@ acc1.requestLoan(1000);
 console.log(acc1.getMovements());
 
 console.log(acc1);
-// console.log(acc1.pin);
 
-// console.log(acc1.#movements); // Property '#movements' is not accessible outside class 'Account' because it has a private identifier.ts(18013)
+// console.log(acc1.#movements);
+// Property '#movements' is not accessible outside class 'Account' because it has a private identifier.ts(18013)
+
+// console.log(acc1.#pin);
+// Property '#pin' is not accessible outside class 'Account' because it has a private identifier.
+
+// console.log(acc1.#approvedLoan(100));
+
+Account.helper();
+*/
 
 // ======================================
 // 022 - Chaining Methods
 // ======================================
+
+/*
+class Account {
+  // 1) Public fields (instances) which means there are all not on the prototype
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    // protected property
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  // requestLoan(val) {
+  //   if (this.#approveLoan(val)) {
+  //     this.deposit(val);
+  //     console.log("Loan Approved");
+  //   }
+  // }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log("Loan Approved");
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log("Helper");
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  //   return true;
+  // }
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account("Jonas", "EUR", 1111);
+// console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+// acc1.approveLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+
+// console.log(acc1.#movements);
+// Property '#movements' is not accessible outside class 'Account' because it has a private identifier.ts(18013)
+
+// console.log(acc1.#pin);
+// Property '#pin' is not accessible outside class 'Account' because it has a private identifier.
+
+// console.log(acc1.#approvedLoan(100));
+
+Account.helper();
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
+*/
+
 // ======================================
 // 023 - ES6 Classes Summary
 // ======================================
+
 // ======================================
 // 024 - Coding Challenge 4
 // ======================================
